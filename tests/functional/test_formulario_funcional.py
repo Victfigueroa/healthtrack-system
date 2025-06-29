9c495b1e4aff98c6337e0be0e699e71e525e7782
Víctor Figueroa <vict.figueroa.u@gmail.com>
2025-06-28 21:39:32 -0400

    levanta el forumulario en el contenedor web

--- tests/functional/test_formulario_funcional.py
+++ tests/functional/test_formulario_funcional.py
@@ -8,8 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 
-# Configurar el driver para usar el Selenium Grid (Docker) en GitHub Actions
-SELENIUM_REMOTE_URL = "http://localhost:4444/wd/hub"
+SELENIUM_REMOTE_URL = os.environ.get("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Remote(
@@ -17,28 +16,23 @@ driver = webdriver.Remote(
     options=options
 )
 
-# URL del formulario servido en el runner (puerto 8000)
-url = "http://localhost:8000/formulario_usuario.html"
-
+# Acceder al formulario desde el contenedor del servicio 'web'
+url = "http://web:8000/formulario_usuario.html"
 driver.get(url)
 
-wait = WebDriverWait(driver, 10)  # espera hasta 10 segundos
+wait = WebDriverWait(driver, 10)
 
-# Esperar y llenar el campo nombre
 nombre_input = wait.until(EC.visibility_of_element_located((By.ID, "nombre")))
 nombre_input.send_keys("Victor")
 
-# Esperar y llenar el campo peso
 peso_input = wait.until(EC.visibility_of_element_located((By.ID, "peso")))
 peso_input.send_keys("75")
 
-# Esperar y clicar el botón
 boton = wait.until(EC.element_to_be_clickable((By.TAG_NAME, "button")))
 boton.click()
 
-# Esperar a que aparezca el resultado
 resultado = wait.until(EC.visibility_of_element_located((By.ID, "resultado"))).text
-
 assert resultado == "Usuario: Victor, Peso actualizado: 75 kg"
 
 driver.quit()
+
